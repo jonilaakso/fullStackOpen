33ddeec5f18151ebd2abd05dc7f22b8b78c08ad1
Joni Laakso <jonilaakso2@gmail.com>
2023-08-15 08:23:43 +0300

    uusi unicafe

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,40 +1,138 @@
 import { useState } from 'react'
 
-const App = () => {
 
-  //tilamuuttujat nappeja varten
+//TODO: 
+
+//Seuraavaksi 1.11 unicafe step6
+//KORJAA AVERAGE JA POSITIVE. Laskun pitäisi tapahtua lisäysten jälkeen, ei samanaikaisesti.
+
+
+//Statistiikat
+const Statistics = (props) => {
+
+  return(
+    <>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="Good:" value={ props.good } />
+          <StatisticLine text="Neutral:" value={ props.neutral } />
+          <StatisticLine text="Bad:" value={ props.bad } />
+          <StatisticLine text="All:" value={ props.all } />
+          <StatisticLine text="Average:" value={ props.average } />
+          <StatisticLine text="Positive:" value={ props.positive } />
+        </tbody>
+      </table>
+
+    </>
+  );
+}
+
+const StatisticLine = (props) => {
+  if (props.text === "Average:") {
+    return (
+      <tr>
+        <td>{ props.text }</td>
+        <td>{ props.value.toFixed(1) } </td>
+      </tr>
+    )
+  }
+  else if (props.text === "Positive:"){
+    return(
+      <tr>
+        <td>{ props.text }</td>
+        <td>{ props.value.toFixed(1) } % </td>
+      </tr>
+    )
+
+  }
+
+  else{
+    return (
+      <tr>
+        <td>{ props.text }</td>
+        <td>{ props.value }</td>
+      </tr>
+    )
+  }
+
+}
+
+//Button component
+const Button = (props) => {
+  
+  return(
+    <button onClick={props.handleClick}>
+      { props.text }
+    </button>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
   const [average, setAverage] = useState(0)
   const [positive, setPositive] = useState(0)
+ 
 
-  /* TODO: Laajenna sovellusta siten, että se näyttää palautteista enemmän statistiikkaa: yhteenlasketun määrän, keskiarvon (hyvän arvo 1, neutraalin 0, huonon -1) ja sen kuinka monta prosenttia palautteista on ollut positiivisia: */
+  const updateStats = (name) => {
+
+    switch (name){
+
+      case "Good":
+        const updatedGood = good + 1;
+        setGood(updatedGood);
+        break;
+      case "Neutral":
+        const updatedNeutral = neutral + 1;
+        setNeutral(updatedNeutral);
+        break;
+      case "Bad":
+        const updatedBad = bad + 1;
+        setBad(updatedBad);
+        break;
+      default: 
+        break;
+    }
+
+    //set all
+    const updatedAll = all + 1;
+    setAll(updatedAll);
+    setAverage((good - bad) / updatedAll);
+    setPositive((good / updatedAll * 100));
+  }
+  
+
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={ () => setGood(good + 1)}>
-        Good 
-      </button>
-    
+      <h1>Give Feedback</h1>
+   
+      <Button handleClick={() => {
+        updateStats("Good");
+        }} 
+        text="Good" /> 
 
-      <button onClick={ () => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-     
+      <Button handleClick={() => {
+        updateStats("Neutral");
+      }} text="Neutral" /> 
 
-      <button onClick={ () => setBad(bad +1)}>
-        bad
-      </button>
-      
+      <Button handleClick={() => {
+        updateStats("Bad");
+      }} text="Bad" /> 
 
-      <h1>statistics</h1>
-      <p>Good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+
+      <Statistics 
+      good = { good }
+      neutral = { neutral }
+      bad = { bad }
+      all = { all }
+      average = { average }
+      positive = { positive }
+      />
     </div>
   )
 }
